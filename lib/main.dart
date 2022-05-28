871006143c528b8ac258edc4e344feff9a304e53
Navaneeth Krishna <navaneeth.at.github@example.com>
2022-05-28 14:43:17 -0700

    making questions

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,19 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+
+var questionIndex = 0;
+
+var questions = [{'question':'what is your name',},];
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
